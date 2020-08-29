5b51fc650890551033df82b73e33f5c0a49ec546
alec <alec@joinclyde.com>
2020-08-29 12:14:03 -0400

    WIP Typescript errors

--- src/AlecTests/Test1.ts
+++ src/AlecTests/Test1.ts
@@ -1,18 +1,40 @@
 import * as PIXI from 'pixi.js';
 
-let pixi = new PIXI.Application({
-    width: 256,         // default: 800
-    height: 256,        // default: 600
-    antialias: true,    // default: false
-    transparent: false, // default: false
-    resolution: 1       // default: 1
-  }
-);
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application();
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+// load the texture we need
+app.loader.add('pants', 'pants.jpg').load((loader: any, resources: any) => {
+  // This creates a texture from a 'bunny.png' image
+  const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+  // Setup the position of the bunny
+  bunny.x = app.renderer.width / 2;
+  bunny.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building
+  app.stage.addChild(bunny);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+     // each frame we spin the bunny around a bit
+    bunny.rotation += 0.01;
+  });
+});
 
 const aa = () => {
-  console.log('aa')
-  document.body.appendChild(pixi.view);
-  pixi.stage.addChild(PIXI.Sprite.from('something.png'));
+  // console.log('aa')
+  // document.body.appendChild(pixi.view);
 }
 
 export default aa;
