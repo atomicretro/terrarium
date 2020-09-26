850de243aef1cc6aba793dfc80cb1f91883c5532
Anthony Lui <anthony.lui@mediamonks.com>
2020-09-26 12:34:30 +0200

    add gsap and sample movement

--- src/components/App.ts
+++ src/components/App.ts
@@ -2,7 +2,7 @@ import * as PIXI from 'pixi.js';
 import Creature from "./Creature";
 
 const App = () => {
-  const PixiApp = new PIXI.Application({width: 800, height: 450});
+  const PixiApp = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
   const genghisHawk = new Creature(PixiApp, '../assets/creatures/genghis_hawk/genghis_hawk.json');
   document.body.appendChild(PixiApp.view);
 }
--- src/components/Creature.ts
+++ src/components/Creature.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import gsap from 'gsap';
 
 export default class Creature {
   private app: PIXI.Application;
@@ -14,10 +15,26 @@ export default class Creature {
 
     this.creature = new PIXI.AnimatedSprite(sheet.animations.creature)
 
-    this.creature.animationSpeed = 0.167; // 6 fps
-    this.creature.position.set(0, 0);
+    this.creature.animationSpeed = 8 / 60; // 6 fps
+    this.creature.anchor.set(0.5);
+    this.creature.position.set(this.creature.width, this.app.view.height - this.creature.height);
     this.creature.play();
 
     this.app.stage.addChild(this.creature);
+    this.move();
+  }
+
+  move() {
+    gsap.to(this.creature.position, {
+      x: this.creature.position.x + 100,
+      y: this.creature.position.y - 100,
+      duration: 1,
+      repeat: -1,
+      onRepeat: () => {
+        console.log("yo")
+        // wasn't able to update position yet after on repeat.
+        this.creature.position.set(this.creature.position.x, this.creature.position.y)
+      }
+    })
   }
 }
