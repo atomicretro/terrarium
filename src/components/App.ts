14362708718f0971bde9456866357a44cf80c35f
alec <alec@joinclyde.com>
2020-10-18 11:43:14 -0400

    Add dick guy

--- src/components/App.ts
+++ src/components/App.ts
@@ -2,6 +2,7 @@ import * as PIXI from 'pixi.js';
 
 import GenghisHawk from '../creatures/GenghisHawk';
 import MouthGuy from '../creatures/MouthGuy';
+import DickGuy from '../creatures/DickGuy';
 
 const App = () => {
   const PixiApp = new PIXI.Application({
@@ -12,7 +13,8 @@ const App = () => {
   // Load sprite and JSON files into app
   PIXI.Loader.shared
     .add('genghis_hawk', '../assets/creatures/genghis_hawk/genghis_hawk.json')
-    .add('mouth_guy', '../assets/creatures/mouth_guy/mouth_guy.json');
+    .add('mouth_guy', '../assets/creatures/mouth_guy/mouth_guy.json')
+    .add('dick_guy', '../assets/creatures/dick_guy/dick_guy.json');
   // Function that runs on load / error of each individual resource
   PIXI.Loader.shared.onProgress.add((loader: PIXI.Loader, resource: Partial<Record<string, PIXI.LoaderResource>>) => {
     console.log(`Loading ${resource.name}`);
@@ -21,6 +23,7 @@ const App = () => {
   PIXI.Loader.shared.load((loader: PIXI.Loader, resources: Partial<Record<string, PIXI.LoaderResource>>) => {
     new GenghisHawk(PixiApp, resources);
     new MouthGuy(PixiApp, resources);
+    new DickGuy(PixiApp, resources);
   });
 
   document.body.appendChild(PixiApp.view);
