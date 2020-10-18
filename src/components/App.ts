d590319495a769106fa411084be79c3e8f3e1493
alec <alec@joinclyde.com>
2020-10-18 12:11:17 -0400

    Reorganize App to be neater

--- src/components/App.ts
+++ src/components/App.ts
@@ -6,19 +6,13 @@ import HissGuy from '../creatures/HissGuy';
 import MouthGuy from '../creatures/MouthGuy';
 import SpermMan from '../creatures/SpermMan';
 
-const gameLoop = (x: any, y: any, z: any) => {
-  console.log('x', x)
-  console.log('y', y)
-  console.log('z', z)
-};
+const PixiApp = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0x986ef,
+});
 
-const App = () => {
-  const PixiApp = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: 0x986ef,
-  });
-
+const gameLoad = () => {
   // Load sprite and JSON files into app
   PIXI.Loader.shared
     .add('dick_guy', '../assets/creatures/dick_guy/dick_guy.json')
@@ -27,25 +21,38 @@ const App = () => {
     .add('mouth_guy', '../assets/creatures/mouth_guy/mouth_guy.json')
     .add('sperm_man', '../assets/creatures/sperm_man/sperm_man.json');
   // Function that runs on load / error of each individual resource
-  PIXI.Loader.shared.onProgress.add((loader: PIXI.Loader, resource: Partial<Record<string, PIXI.LoaderResource>>) => {
-    console.log(`Loading ${resource.name}`);
-  });
+  PIXI.Loader.shared.onProgress.add(onProgress);
   // After all resources are loaded, create creatures
-  PIXI.Loader.shared.load((loader: PIXI.Loader, resources: Partial<Record<string, PIXI.LoaderResource>>) => {
-    new DickGuy(PixiApp, resources);
-    new GenghisHawk(PixiApp, resources);
-    new HissGuy(PixiApp, resources);
-    new MouthGuy(PixiApp, resources);
-    new SpermMan(PixiApp, resources);
+  PIXI.Loader.shared.load(gameSetup);
+}
 
-    console.log('hi', PixiApp)
-    PixiApp.ticker.add((x, y, z) => gameLoop(x, y, z));
-    
-    setTimeout(() => {
-      PixiApp.ticker.stop()
-    }, 3000)
-  });
+const onProgress = (loader: PIXI.Loader, resource: Partial<Record<string, PIXI.LoaderResource>>) => {
+  console.log(`Loading ${resource.name}`);
+};
 
+const gameSetup = (loader: PIXI.Loader, resources: Partial<Record<string, PIXI.LoaderResource>>) => {
+  const creatures = [
+    new DickGuy(PixiApp, resources),
+    new GenghisHawk(PixiApp, resources),
+    new HissGuy(PixiApp, resources),
+    new MouthGuy(PixiApp, resources),
+    new SpermMan(PixiApp, resources),
+  ];
+
+  PixiApp.ticker.add((deltaMs) => gameLoop(deltaMs, creatures));
+
+  // setTimeout(() => {
+  //   PixiApp.ticker.stop()
+  // }, 3000)
+};
+
+const gameLoop = (deltaMs: number, creatures:any[]) => {
+  console.log('deltaMs', deltaMs)
+  console.log('creatures', creatures)
+};
+
+const App = () => {
+  gameLoad();
   document.body.appendChild(PixiApp.view);
 };
 
